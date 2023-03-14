b75194776d2bf4c8a212aaab550c1aa686890b67
Nicholas Arsenault <nickarsenault18@gmail.com>
2023-03-14 19:30:16 -0400

    New php for form submission, connected to Andrew

---
+++ add_task.php
@@ -0,0 +1,34 @@
+<?php
+// add_task.php
+
+// Database connection
+$servername = "localhost";
+$username = "id20153491_testingthis";
+$password = "i]8[EBlzQ[!4!u5s";
+$database = "id20153491_test";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+// Get form data
+$task_desc = $_POST['task_desc'];
+$due_date = $_POST['due_date'];
+$task_category = $_POST['task_category'];
+$priority = $_POST['priority'];
+$status = isset($_POST['status']) ? 'active' : 'completed';
+
+// SQL query to insert new task
+$sql = "INSERT INTO tasks (task_desc, due_date, task_category, priority, status) VALUES ('$task_desc', '$due_date', '$task_category', $priority, '$status')";
+
+if (mysqli_query($conn, $sql)) {
+	// Redirect to home page
+	header("Location: index.php");
+	exit();
+} else {
+	echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
