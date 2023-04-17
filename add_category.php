1265b1466f5b894a6971c90217c05f1748900290
Nicholas Arsenault <nickarsenault18@gmail.com>
2023-04-17 18:13:00 -0400

    Safety changes to mysqli_connect

--- add_category.php
+++ add_category.php
@@ -7,9 +7,9 @@ $username = "id20153491_testingthis";
 $password = "l5ak3jKJ_44hJh]9";
 $database = "id20153491_test";
 
-$conn = mysqli_connect($servername, $username, $password, $database);
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+$conn = mysqli($servername, $username, $password, $database);
+if ($conn->connect_error) {
+    die("Connection failed: " . connect_error());
 }
 
 // Get form data
