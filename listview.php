29046ad088bf6209c6a26daceb86607ee2d4a2d2
EternalBlue14 <124602693+EternalBlue14@users.noreply.github.com>
2023-04-20 00:42:04 -0400

    Updated category and completed task view section
    Still WIP, made some small udpates to the views section, split category views and completed views into two tables. Completed views partially working only need to implement date input field for user to select a date to view completed task

--- listview.php
+++ listview.php
@@ -100,16 +100,13 @@ if(isset($_POST['submit2'])){
           ?>
       </table>
       <br />
-      <h1>Custom Views</h1>
+     <h1>View By Category</h1>
       <?php
         //Queries for category list views
         $taskCatQuery = "SELECT * FROM tasks ORDER BY task_priority ASC, due_date ASC";
         $catQuery = "SELECT category_name FROM categories";
         $taskCatData = mysqli_query($conn, $taskCatQuery) or die('Unable to obtain data: '. mysqli_connect_error());
         $catData = mysqli_query($conn, $catQuery) or die('Unable to obtain data: '. mysqli_connect_error());
-        //Queries for completed tasks list view
-        $taskCompQuery = "SELECT * FROM tasks WHERE task_status = 1 ORDER BY due_date ASC";
-        $taskCompData = mysqli_query($conn, $taskCompQuery) or die('Unable to obtain data: '. mysqli_connect_error());
         
         //Converts category_name column from categories table into array
         $catArray = array();
@@ -126,7 +123,7 @@ if(isset($_POST['submit2'])){
         
         //Creates drop down list to select a list view
         echo '<form method="post" action="">';
-        echo '<label for="view">Select List View: </label>';
+        echo '<label for="view">Select Catgory View</label>';
         echo '<select id="view" name="view">';
         //Loops through the array created above to display available categories
         foreach ($catArray as $value) {
@@ -138,12 +135,61 @@ if(isset($_POST['submit2'])){
         
         //Brings up the selected form with the information
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $selectedView = $_POST['submit'];
-            if (isset($taskCatArray[$selectedView])) {
-                echo '<table>';
+            $selectedView = $_POST['view'];
+            if (isset($catArray[$selectedView])) {
+                
             }
         }
       ?>
+      <br />
+      <h1>View by Completed Tasks</h1>
+      <table>
+          <!--
+          <tr>
+              <th>Description</th>
+              <th>Due Date</th>
+              <th>Category</th>
+              <th>Priority Level</th>
+              <th>Status</th>
+          </tr>
+          -->
+          <?php
+            //Queries for completed tasks list view
+            $taskCompQuery = "SELECT * FROM tasks WHERE task_status = 1 ORDER BY due_date ASC";
+            $taskCompData = mysqli_query($conn, $taskCompQuery) or die('Unable to obtain data: '. mysqli_connect_error());
+            if(empty($taskCompData)){
+                echo "There are no completed tasks to display";
+            }
+            else{
+                echo "<tr>";
+                echo "<th>Descirption</th>";
+                echo "<th>Due Date</th>";
+                echo "<th>Category</th>";
+                echo "<th>Priority Level</th>";
+                echo "<th>Status</th>";
+                echo "</th>";
+                while ($row = mysqli_fetch_array($taskCompData, MYSQLI_ASSOC)){
+                    echo "<tr><form method='post'>";
+                    echo "<td><input type='text' name='task_desc' value='".$row["task_desc"]."'></td>";
+                    echo "<td><input type='date' name='due_date' value='".$row["due_date"]."'></td>";
+                    // Display categories dropdown
+                    echo "<td><select name='task_category_id'>";
+                    $categories_query = mysqli_query($conn, "SELECT * FROM categories ORDER BY category_name ASC");
+                    while ($category_row = mysqli_fetch_array($categories_query, MYSQLI_ASSOC)) {
+                        $selected = $category_row['id'] == $row['task_category_id'] ? 'selected' : '';
+                        echo "<option value='".$category_row['id']."' $selected>".$category_row['category_name']."</option>";
+                    }
+                echo "</select></td>";
+
+                echo "<td><input type='number' id='task_priority' name='task_priority' min='1' max='4' value='".$row["task_priority"]."'></td>";
+                echo "<td><input type='checkbox' name='task_status' value='1' " . ($row["task_status"] == 1 ? "checked" : "") . "></td>";
+                echo "<td><input type='submit' name='submit' value='Save'></td>";
+                echo "<input type='hidden' name='id' value='".$row["id"]."'>";
+                echo "</form></tr>";
+                }
+            }
+          ?>
+      </table>
   </div>
   
 </body>
